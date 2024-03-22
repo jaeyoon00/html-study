c204e4594e7764e3cad9a330f53ba8f18a3aade7
jaeyoon00 <qwero55@naver.com>
2024-03-22 14:03:14 +0900

    list

--- day3/day3.js
+++ day3/day3.js
@@ -1,20 +1,23 @@
 // (list, arr), object
 // int [] arr = new int[123];
 // List<Integer> list = new ArrayList<>();
-const list = [];
+const list = []; // 100... 1000 ->
+const list369 = []; // 369 리스트를 만들자
 // arr[0] = 1;
 // list.add(1);
-for (let index = 1; index <= 50; index++){
-    const num = 
-    (index + "").includes("3") ||
-    (index + "").includes("6") ||
-    (index + "").includes("9") 
+for (let index = 0; index < list.length; index++){
+    // list.push(index);
+    const element = 
+    list[index].toString().includes("3") ||
+    list[index].toString().includes("6") ||
+    list[index].toString().includes("9") 
     ? "짝" 
-    : index;
+    : array[index];
+    list369.push(element);
+}
+console.log(list369);
     // const num = index % 3 === 0 ? "짝" : index;
     // if(index%3 ==0) list.push("짝");
     // else list.push(index);
-    list.push(num);
-}
-console.log(list);
+    // list.push(num);
 // console.log(list.length);
