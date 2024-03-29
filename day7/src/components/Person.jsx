d4c034b90b0e9b392560693d265d3e3b908ea1b9
jaeyoon00 <qwero55@naver.com>
2024-03-29 15:05:30 +0900

    react

---
+++ day7/src/components/Person.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const Person = ({ age }) => {
+  const [name, setName] = useState("송희");
+
+  return (
+    <>
+      <input onChange={(e) => setName(e.target.value)}></input>
+      <p>이름 : {name}</p>
+      <p>나이 : {age}</p>
+    </>
+  );
+};
+export default Person;
