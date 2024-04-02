0bc95c2548dca9bde9642ef5b58f141819c04b3b
jaeyoon00 <qwero55@naver.com>
2024-04-02 16:06:14 +0900

    props

---
+++ day9/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './pages/Counter';
+import Signup from './pages/Signup';
+
+
+
+
+
+function App() {
+
+  return (
+    
+    <Signup/>
+    
+  );
+}
+
+export default App
---
+++ day9/src/pages/Counter.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import ViewCount from "../components/Viewcount";
+import ChangeCount from "../components/ChangeCount";
+import LogCount from "../components/LogCount";
+
+const Counter = () => {
+  //   const [count, setCount] = useState(0);
+  const [logs, setLogs] = useState([]);
+
+  return (
+    <div className="container">
+      <ViewCount />
+      <ChangeCount />
+      <hr className="m-4" />
+      <LogCount logs={logs} />
+    </div>
+  );
+};
+
+export default Counter;
